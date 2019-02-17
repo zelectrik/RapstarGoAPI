acfcaa1038742a0d27b149561feec1ec3b31c0f0
Thibault DUFOUR <t.dufour740@gmail.com>
2019-02-17 16:05:01 -0500

    DB connection

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-var http = require('http');
+var express = require('express');
+var mongo = require('mongodb');
+var app = express();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World! XD');
-}).listen(3000);
+var MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb://127.0.0.1:27017/";
+
+var dbo;
+
+MongoClient.connect(uri, {
+  useNewUrlParser: true,
+  server : {
+    auto_reconnect : true
+  }
+}, function(err, db) {
+  if (err) throw err;
+  dbo = db.db("local");
+
+  app.listen(3000);
+  console.log("Listening on port 3000.");
+  console.log(dbo);
+});
+
+// Reuse database object in request handlers
+app.get("/", function(req, res) {
+  dbo.collection('character').find({}).toArray(function(err, val) {
+    if (err) throw err;
+    console.log(val[0].abilities);
+    res.end();
+  });
+});
