ea0541145783c164edd41da39047db340479d4f8
Thibault DUFOUR <t.dufour740@gmail.com>
2019-02-17 22:27:34 -0500

    Socket.io support

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
-var mongo = require('mongodb');
+var bodyParser = require('body-parser');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongo = require('mongodb');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 var MongoClient = require('mongodb').MongoClient;
 var uri = "mongodb://127.0.0.1:27017/";
@@ -16,16 +22,58 @@ MongoClient.connect(uri, {
   if (err) throw err;
   dbo = db.db("local");
 
-  app.listen(3000);
+  http.listen(3000);
   console.log("Listening on port 3000.");
   console.log(dbo);
 });
 
-// Reuse database object in request handlers
-app.get("/", function(req, res) {
-  dbo.collection('character').find({}).toArray(function(err, val) {
-    if (err) throw err;
-    console.log(val[0].abilities);
-    res.end();
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+
+
+  socket.on('login', function(data) {
+    dbo.collection('user').findOne({socket_id : socket.id}, function(err, val) {
+      if (err) console.log(err);
+      if(val != null && val.pseudo != data.pseudo)
+      {
+        dbo.collection('user').updateOne(val, {$set : {socket_id : ""}},{}, function(err) {
+          if(err) console.log(err);
+          console.log("Disconnect from " + val.pseudo);
+        });
+      }
+      dbo.collection('user').findOne({pseudo : data.pseudo, password : data.password}, function(err, val) {
+        if (err) console.log(err);
+        if(val == null)
+        {
+          console.log("Mauvais pseudo ou mot de passe.");
+          socket.emit('message', {message : "Mauvais pseudo ou mot de passe."});
+        } else {
+          dbo.collection('user').updateOne(val, {$set : {socket_id : socket.id}},{}, function(err) {
+            if(err) console.log(err);
+            console.log("Connect to " + val.pseudo);
+            socket.emit('message', {message : "Connect to " + val.pseudo});
+          });
+        }
+      });
+    });
+  });
+
+  socket.on('loggedAccount', function(data) {
+    dbo.collection('user').findOne({socket_id : socket.id}, function(err, val) {
+      if (err) console.log(err);
+      if(val == null)
+      {
+        console.log("Connecté à aucun compte.");
+        socket.emit('message', {message : "Connecté à aucun compte."});
+      } else {
+        console.log("Connecté à " + val.pseudo);
+        socket.emit('message', {message : "Connecté à " + val.pseudo});
+      }
+    });
+  });
+
+  socket.on('disconnect', function (socket) {
+    console.log("A client has disconnected!");
   });
 });
