56ec1ca474aed41b6a1dd1b2ab0b9a06b230ec1f
Thibault DUFOUR <t.dufour740@gmail.com>
2019-02-27 18:57:43 -0500

    Clean code

--- app.js
+++ app.js
@@ -37,111 +37,128 @@ io.on('connection', function(socket) {
 
   /* Login Function */
   socket.on('login', function(data) {
-    dbo.collection('user').findOne({socket_id : socket.id}, function(err, val) {
-      if (err) console.log(err);
-      /* Permit to disconnect user if use this socket for test */
-      if(val != null && val.pseudo != data.pseudo)
-      {
-        dbo.collection('user').updateOne(val, {$set : {socket_id : ""}},{}, function(err) {
-          if(err) console.log(err);
-          console.log("Disconnect from " + val.pseudo);
-        });
-      }
-
-      /* Check if login and password are good */
-      dbo.collection('user').findOne({pseudo : data.pseudo, password : data.password}, function(err, val) {
-        if (err) console.log(err);
-        /*Wrong connection*/
-        if(val == null)
-        {
-          console.log("Mauvais pseudo ou mot de passe.");
-          socket.emit('loginResult', {
-            success : false,
-            body : {
-              message : "Mauvais pseudo ou mot de passe."
-            }});
-        }
-        /*Good connection*/
-        else {
-          dbo.collection('user').updateOne(val, {$set : {socket_id : socket.id}},{}, function(err) {
-            if(err) console.log(err);
-            console.log("Connect to " + val.pseudo);
-            socket.emit('loginResult', {
-              success : true,
-              body : {
-                message : "Connect to " + val.pseudo,
-                socket_id : socket.id,
-                pseudo : val.pseudo
-              }});
-          });
-        }
-      });
-    });
+    login(data, socket);
   });
 
   socket.on('reconnection', function(data) {
-    /* Not need to be reconnect if socket_id unchanged */
-    if(data.socket_id == socket.id)
-    {
-      socket.emit('reconnectionResult', {
-        success : true,
-        body : {
-          new_socket_id : false,
-          message : "Not needed reconnection"
-        }});
-    } else {
-      dbo.collection('user').findOne({pseudo : data.pseudo ,socket_id : data.socket_id}, function(err, val) {
-        if (err) console.log(err);
-        /* Permit to disconnect user if use this socket for test */
-        if(val != null)
-        {
-          dbo.collection('user').updateOne(val, {$set : {socket_id : socket.id}},{}, function(err) {
-            if(err) console.log(err);
-            console.log("Reconnection from " + val.pseudo);
-            socket.emit('reconnectionResult', {
-              success : true,
-              body : {
-                new_socket_id : true,
-                message : "Connect to " + val.pseudo,
-                socket_id : socket.id
-              }});
-          });
-        } else {
-          console.log("Can't reconnect ");
-          socket.emit('reconnectionResult', {
-            success : false,
-            body : {
-              message : "Can't reconnect."
-            }});
-        }
-      });
-    }
+    reconnection(data, socket)
   });
 
 
   socket.on('loggedAccount', function() {
-    dbo.collection('user').findOne({socket_id : socket.id}, function(err, val) {
-      if (err) console.log(err);
-      if(val == null)
-      {
-        console.log("Connecté à aucun compte.");
-        socket.emit('loggedAccountResult', {
-            success : false,
-            body : {
-              message : "Not connected."
-            }});
-      } else {
-        console.log("Connecté à " + val.pseudo);
-        socket.emit('loggedAccountResult', {
-            success : true,
-            body : {
-              message : "Connected to " + val.pseudo
-            }});
-      }
-    });
+    loggedAccount(data, socket)
   });
 
   socket.on('disconnect', function (socket) {
     console.log("A client has disconnected!");
   });
 });
+
+
+
+function login(data, socket)
+{
+  dbo.collection('user').findOne({socket_id : socket.id}, function(err, val) {
+    if (err) console.log(err);
+    /* Permit to disconnect user if use this socket for test */
+    if(val != null && val.pseudo != data.pseudo)
+    {
+      dbo.collection('user').updateOne(val, {$set : {socket_id : ""}},{}, function(err) {
+        if(err) console.log(err);
+        console.log("Disconnect from " + val.pseudo);
+      });
+    }
+
+    /* Check if login and password are good */
+    dbo.collection('user').findOne({pseudo : data.pseudo, password : data.password}, function(err, val) {
+      if (err) console.log(err);
+      /*Wrong connection*/
+      if(val == null)
+      {
+        console.log("Mauvais pseudo ou mot de passe.");
+        socket.emit('loginResult', {
+          success : false,
+          body : {
+            message : "Mauvais pseudo ou mot de passe."
+          }});
+      }
+      /*Good connection*/
+      else {
+        dbo.collection('user').updateOne(val, {$set : {socket_id : socket.id}},{}, function(err) {
+          if(err) console.log(err);
+          console.log("Connect to " + val.pseudo);
+          socket.emit('loginResult', {
+            success : true,
+            body : {
+              message : "Connect to " + val.pseudo,
+              socket_id : socket.id,
+              pseudo : val.pseudo
+            }});
+        });
+      }
+    });
+  });
+}
+
+function reconnection(data, socket)
+{
+  /* Not need to be reconnect if socket_id unchanged */
+  if(data.socket_id == socket.id)
+  {
+    socket.emit('reconnectionResult', {
+      success : true,
+      body : {
+        new_socket_id : false,
+        message : "Not needed reconnection"
+      }});
+  } else {
+    dbo.collection('user').findOne({pseudo : data.pseudo ,socket_id : data.socket_id}, function(err, val) {
+      if (err) console.log(err);
+      /* Permit to disconnect user if use this socket for test */
+      if(val != null)
+      {
+        dbo.collection('user').updateOne(val, {$set : {socket_id : socket.id}},{}, function(err) {
+          if(err) console.log(err);
+          console.log("Reconnection from " + val.pseudo);
+          socket.emit('reconnectionResult', {
+            success : true,
+            body : {
+              new_socket_id : true,
+              message : "Reconnect to " + val.pseudo,
+              socket_id : socket.id
+            }});
+        });
+      } else {
+        console.log("Can't reconnect ");
+        socket.emit('reconnectionResult', {
+          success : false,
+          body : {
+            message : "Can't reconnect."
+          }});
+      }
+    });
+  }
+}
+
+function loggedAccount(data, socket)
+{
+  dbo.collection('user').findOne({socket_id : socket.id}, function(err, val) {
+    if (err) console.log(err);
+    if(val == null)
+    {
+      console.log("Connecté à aucun compte.");
+      socket.emit('loggedAccountResult', {
+          success : false,
+          body : {
+            message : "Not connected."
+          }});
+    } else {
+      console.log("Connecté à " + val.pseudo);
+      socket.emit('loggedAccountResult', {
+          success : true,
+          body : {
+            message : "Connected to " + val.pseudo
+          }});
+    }
+  });
+}
