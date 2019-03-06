6e0eb2d6cb443cce41cf686d84db3ff98fbc6721
Thibault DUFOUR <t.dufour740@gmail.com>
2019-03-05 19:27:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -347,6 +347,7 @@ function GetAllMyCharacters(data, socket)
       socket.emit('getAllMyCharactersResult', {
           success : true,
           body : {
+            list : JSON.stringify(val),
             message : "Get all character."
           }});
     }
